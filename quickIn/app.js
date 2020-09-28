f08bd5071539a7bc1ba8bb22cb779e22978a4a36
程99522 <18715518190@163.com>
2020-09-28 19:29:01 +0800

    20200928.01

--- quickIn/app.js
+++ quickIn/app.js
@@ -2,6 +2,30 @@
 App({
   onLaunch: function () {
     wx.removeStorageSync('keyWord');
+
+    wx.cloud.init();
+        const promise = new Promise((resolve, reject) => {
+            wx.cloud.callFunction({
+                name: 'login',
+                success: (res) => {
+                    console.log(res); 
+                    if(res.result.userInfo){
+                        const { openId } = res.result.userInfo; 
+                        resolve(openId);
+                    }else{
+                        const { openid } = res.result; 
+                        resolve(openid);
+                    }
+                    
+                    
+                },
+                fail: () => {
+                    wx.showToast({
+                        title: '登录失败, 请重新登录',
+                    });
+                },
+            })
+        });
   },
   globalData: {
     userInfo: null
